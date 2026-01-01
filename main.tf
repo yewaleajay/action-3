bc239e9747ff3233bfd36c6cf0ee472d6745f886
yewaleajay <70324721+yewaleajay@users.noreply.github.com>
2026-01-01 17:25:58 +0530

    [skip_ci]

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+## this is main.tf
 resource "google_storage_bucket" "my-bucket" {
   name          = var.bucket_name
   location      = var.location
   project = var.project
   force_destroy = true
   public_access_prevention = "enforced"
+
 }
